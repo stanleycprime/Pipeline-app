f0f432da4d97cb34a12c794777d7f228a7c47070
stanleycprime <stanleycprime@gmail.com>
2025-02-23 22:46:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js!');
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
